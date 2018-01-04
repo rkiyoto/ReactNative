0f2b1b70b2ccbc78ac705522f9feda0bcde2fcd3
Rafael Kiyoto <rafa.kiyoto@gmail.com>
2018-01-04 15:40:56 -0200

    Create App.js

---
+++ Introducao/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'CATIAU,\n' +
+    'Instruções do iPhone',
+  android: 'Instruções do Android', /* Altere aqui */
+});
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello World!
+        </Text>
+        <Text style={styles.instructions}>
+          Já mudamos o stylesheet e o texto das instruções
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF0000',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+    color: 'yellow'
+  },
+});
